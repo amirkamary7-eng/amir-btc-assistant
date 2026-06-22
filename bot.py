b768b82c58558a49ceaec2447e206dc643d2888b
Amir <amirkamary7@gmail.com>
2026-06-22 11:29:47 +0330

    first commit

---
+++ bot.py
@@ -0,0 +1,37 @@
+from telegram import (
+    Update,
+    KeyboardButton,
+    ReplyKeyboardMarkup,
+    WebAppInfo
+)
+
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes
+)
+
+TOKEN = "8989227188:AAGg504dd7By2MF3Wl_YT-HHcoBwGX3lCpw"
+
+WEBAPP_URL = "https://example.com"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    keyboard = [[
+        KeyboardButton(
+            text="🚀 Open App",
+            web_app=WebAppInfo(url=WEBAPP_URL)
+        )
+    ]]
+
+    await update.message.reply_text(
+        "به Amir BTC Assistant خوش آمدی",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard,
+            resize_keyboard=True
+        )
+    )
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
